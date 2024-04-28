586ee581dc78473fc658d6f44b12072abe9b5fbb
ductho05 <ductho23pro@gmail.com>
2024-04-29 00:54:56 +0700

    vetify google

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,19 +1,19 @@
 export default function sitemap() {
     return [
         {
-            url: 'https://taxi-buon-ma-thuot.io.vn',
+            url: 'https://taxi-daklak-giare.io.vn/',
             lastModified: new Date(),
             changeFrequency: 'yearly',
             priority: 1,
         },
         {
-            url: 'https://taxi-buon-ma-thuot.io.vn/about',
+            url: 'https://taxi-daklak-giare.io.vn/about',
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 0.8,
         },
         {
-            url: 'https://taxi-buon-ma-thuot.io.vn/contact',
+            url: 'https://taxi-daklak-giare.io.vn/contact',
             lastModified: new Date(),
             changeFrequency: 'weekly',
             priority: 0.5,
